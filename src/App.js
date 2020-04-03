7d7e16a7663777491be47c7928df059fd8cfa0e3
Gianluca Ciccarelli <sturmer@gergel.im>
2020-04-03 17:05:32 +0300

    List categories. Add button not working yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const costs = [
+  { name: "utilities", cost: 13.9 },
+  { name: "housing", cost: 21.4 },
+  { name: "foodDelivery", cost: 5.6 }
+];
+
+function createCategory() {
+  // TODO Dialog with name; then add row to table CategoryPanel
+}
 
 function App() {
+  // TODO get categories from DB table
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <CategoryPanel />
+      <button onClick={createCategory}>New</button>
+    </>
+  );
+}
+
+function CategoryPanel() {
+  return (
+    <div className="ExpenseCategory">
+      <table>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {costs.map(c => (
+            <Category name={c.name} totalCost={c.cost} />
+          ))}
+        </tbody>
+      </table>
     </div>
+  );
+}
+
+function Category(props) {
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.totalCost}</td>
+    </tr>
   );
 }
 
